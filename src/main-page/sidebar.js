200bea2ff7cd94c9d688f4acc6abc5c731d91328
Greg Atemi <56564097+greg-atemi@users.noreply.github.com>
2023-09-03 18:05:07 +0300

    Refactoring Sidebar navigations to use dynamic array paths

--- src/main-page/sidebar.js
+++ src/main-page/sidebar.js
@@ -5,23 +5,26 @@ import home from "../images/home.svg"
 import sales from "../images/sales.svg"
 import stock from "../images/stock.svg"
 import report from "../images/report.svg"
+import { Link } from 'react-router-dom';
 
 function NavList(){
     const links = [
-        {id: 1, name: "Dashboard" ,src: home},
-        {id: 2, name: "Product" ,src: box},
-        {id: 3, name: "POS" ,src: pos},
-        {id: 4, name: "Stock" ,src: stock},
-        {id: 5, name: "Sales" ,src: sales},
-        {id: 6, name: "Daily Reports" ,src: report},
-        {id: 7, name: "Weekly Reports" ,src: report},
-        {id: 8, name: "Monthly Reports" ,src: report}
+        {id: 1, name: "Dashboard", path:"/", src: home},
+        {id: 2, name: "Product", path:"/products", src: box},
+        {id: 3, name: "POS", src: pos},
+        {id: 4, name: "Stock", src: stock},
+        {id: 5, name: "Sales", src: sales},
+        {id: 6, name: "Daily Reports", src: report},
+        {id: 7, name: "Weekly Reports", src: report},
+        {id: 8, name: "Monthly Reports", src: report}
     ];
 
     const linkItems = links.map(link =>
-        <button key={link.id} className="">
-            <img alt={link.name} className="nav-logo" src={link.src}/>{link.name}
-        </button>
+        <Link to={link.path}>
+            <button key={link.id} className="">
+                <img alt={link.name} className="nav-logo" src={link.src}/>{link.name}
+            </button>
+        </Link>
     );
 
     return (
